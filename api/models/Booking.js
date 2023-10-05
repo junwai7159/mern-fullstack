40ee0b82a52d94fb20ac7420c6b49a18d70b5f2c
junwai7159 <junwai7159@gmail.com>
2023-10-05 19:13:38 +0800

    Initial commit

---
+++ api/models/Booking.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const BookingSchema = new mongoose.Schema ({
+    place: {type:mongoose.Schema.Types.ObjectId, required:true, ref:'Place'},
+    user: {type:mongoose.Schema.Types.ObjectId, required:true},
+    checkIn: {type:Date, required:true},
+    checkOut: {type:Date, required:true},
+    numberOfGuests: {type:Number, required:true},
+    name: {type:String, required:true},
+    phone: {type:String, required:true},
+    price: Number
+});
+
+const BoookingModel = mongoose.model('Booking', BookingSchema);
+
+module.exports = BoookingModel;
