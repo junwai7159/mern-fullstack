9e929049ef63ee369aa0a0834c837ac6f16e8115
Chiang Jun Wai <67097978+junwai7159@users.noreply.github.com>
2023-10-30 23:42:01 +0800

    Update Place.js

--- api/models/Place.js
+++ api/models/Place.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const PlaceSchema = new mongoose.Schema ({
     owner: {type:mongoose.Schema.Types.ObjectId, ref:'User'},
-    title: {type:String, maxlength: 100},
-    address: String,
+    title: {type:String, maxlength: 100, required:true},
+    address: {type:String, required:true},
     photos: [String],
     description: String,
     perks: [String],
@@ -11,7 +11,7 @@ const PlaceSchema = new mongoose.Schema ({
     checkIn: String, 
     checkOut: String,
     maxGuests: {type:Number, min: 1, max: 50},
-    price: {type:Number, min: 1},
+    price: {type:Number, min: 1, required:true},
 });
 
 const PlaceModel = mongoose.model('Place', PlaceSchema);
