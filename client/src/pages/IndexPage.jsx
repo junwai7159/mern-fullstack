4db55995733785e5f49939e6ffcbce803b532ab7
junwai7159 <junwai7159@gmail.com>
2023-10-30 17:39:13 +0800

    h2 to h3

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -18,8 +18,8 @@ export default function IndexPage() {
                 <img className="rounded-2xl object-cover aspect-square" src={'http://localhost:3000/uploads/' + place.photos?.[0]} />
               )}
             </div>
-            <h2 className="font-bold">{place.address}</h2>
-            <h3 className="text-sm truncate text-gray-500">{place.title}</h3>
+            <h2 className="font-bold">{place.title}</h2>
+            <h3 className="text-sm truncate text-gray-500">{place.address}</h3>
             <div className="mt-1">
               <span className="font-bold">{place.price} 元 / 每晚</span>
             </div>
