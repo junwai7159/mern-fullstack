124e37e2af06576fb0799fdcc898aa23ec91d920
Chiang Jun Wai <67097978+junwai7159@users.noreply.github.com>
2023-10-30 23:28:39 +0800

    Update Booking.js

--- api/models/Booking.js
+++ api/models/Booking.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const BookingSchema = new mongoose.Schema ({
     place: {type:mongoose.Schema.Types.ObjectId, required:true, ref:'Place'},
-    user: {type:mongoose.Schema.Types.ObjectId, required:true},
+    user: {type:mongoose.Schema.Types.ObjectId, required:true, ref:'User'},
     checkIn: {type:Date, required:true},
     checkOut: {type:Date, required:true},
     numberOfGuests: {type:Number, required:true},
